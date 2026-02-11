d0d69e9db12db910ff9af4ec1d9e36fa9893f42a
arcofs <tom@arcofs.com.au>
2026-02-11 19:20:57 +0800

    fix firebase key

--- api/src/main.ts
+++ api/src/main.ts
@@ -58,13 +58,22 @@ async function bootstrap() {
     type: 'service_account',
     projectId: process.env.FIREBASE_PROJECT_ID,
     privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    privateKey: process.env.FIREBASE_PRIVATE_KEY
+      ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') // Replace literal \n with newlines
+        .replace(/^"|"$/g, '') // Remove surrounding double quotes if present
+        .replace(/^'|'$/g, '') // Remove surrounding single quotes if present
+      : undefined,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     clientId: process.env.FIREBASE_CLIENT_ID,
     authUri: 'https://accounts.google.com/o/oauth2/auth',
     tokenUri: 'https://oauth2.googleapis.com/token',
     authProviderX509CertUrl: 'https://www.googleapis.com/oauth2/v1/certs',
-    clientC509CertUrl: process.env.FIREBASE_CLIENT_C509_CERT_URL,
+    clientC509CertUrl: process.env.FIREBASE_CLIENT_C509_CERT_URL
+      ? process.env.FIREBASE_CLIENT_C509_CERT_URL.replace(/["',]+$/g, '').replace(
+        /^["']+/g,
+        '',
+      )
+      : undefined,
   }
 
   firebase.initializeApp({
